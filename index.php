c531daa4ba3b11a2dd1a7db2a44c84b341a97123
juaum <joaopedro.sampaio25012@gmail.com>
2018-08-24 13:09:55 +0000

    Modificando o Index (Correto)

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewpiont" content="width=device-width, initial-scale-1.0">
+            <meta http-equip="X-UA Compatible" content="ie-edge">
+            <title>Document</title>
+        </head>
+        <body>
+            <h2>Seja benvindo ao meu novo tema</h2>
+        </body>
+    </html>
