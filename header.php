90df3b097a2d53f56480f2bb7d9ebe2c4e6590e5
juaum <joaopedro.sampaio25012@gmail.com>
2018-08-24 13:41:55 +0000

    Wordpress - Parte 2

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewpiont" content="width=device-width, initial-scale-1.0">
+            <meta http-equip="X-UA Compatible" content="ie-edge">
+            <title><?php wp_title();?></title>
+            <link rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>" type="text/css" />
+            
+            <?php wp_head();?>
+            
+        </head>
+        <body>
+           
+       
--- index.php
+++ index.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta name="viewpiont" content="width=device-width, initial-scale-1.0">
-            <meta http-equip="X-UA Compatible" content="ie-edge">
-            <title>Document</title>
-        </head>
-        <body>
+
+            <?php get_header();?>
             <h2>Seja benvindo ao meu novo tema</h2>
-        </body>
-    </html>
+            <?php get_footer();?>
